f891b4fc4d5820ad1f01cb1e3c99766c06d017b6
Lucas de Paula <lucas.barbosa@microcity.com.br>
2019-06-11 17:42:45 -0300

    Configuração de métodos REST

--- src/controllers/personController.js
+++ src/controllers/personController.js
@@ -1,25 +1,58 @@
-exports.get = (req, res, next) => {
-    res.status(200).send('Requisição GET recebida com sucesso!');
-};
 
+let prodList = { items: [] }
+let lista = prodList.items;
+
+exports.get = (req, res, next) => {
+    res.json(prodList);
+};
 
 exports.getById = (req, res, next) => {
-    res.status(200).send('Requisição GET recebida com sucesso!');
+    let param_id = req.params.id;
+
+    for (let i = 0; i < lista.length; i++) {
+        if(lista[i].id == param_id){
+            res.json(lista[i]);
+        }
+    }
 };
-
-
 
 exports.post = (req, res, next) => {
-    res.status(201).send('Requisição POST recebida com sucesso!');
+    prodList.items.push(req.body)
+    res.end('Produto criado!');
 };
 
-
 exports.put = (req, res, next) => {
-    let id = req.params.id;
-    res.status(204).send(`Requisição PUT recebida com sucesso! ${id}`);
+    let param_id = req.params.id;
+    let param_body = req.body;
+    
+    for (let x = 0; x < lista.length; x++) {
+        if(lista[x].id == param_id){
+            if(param_body.tipo != undefined){
+                lista[x].tipo = param_body.tipo;
+            }
+            if(param_body.nome != undefined){
+                lista[x].nome = param_body.nome;
+            }
+            if(param_body.fabricante != undefined){
+                lista[x].fabricante = param_body.fabricante;
+            }
+            if(param_body.descricao != undefined){
+                lista[x].descricao = param_body.descricao;
+            }
+            
+            res.end('Produto atualizado!');
+        }
+    }
 };
 
 exports.delete = (req, res, next) => {
-    let id = req.params.id;
-    res.status(204).send(`Requisição DELETE recebida com sucesso! ${id}`);
+    let param_id = req.params.id;
+
+    for (let i = 0; i < lista.length; i++) {
+        if(lista[i].id == param_id){
+            lista.splice(i,1);
+            i--;
+            res.end('Produto excluído!');
+        }
+    }
 };
